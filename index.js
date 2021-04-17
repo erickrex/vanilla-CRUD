b5b0a15d7c1b817d5111c32d4981d656c796c910
Erick desktop <erickrhein@gmail.com>
2021-04-17 09:39:26 +0200

    Html and basic styles from bootstrap

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //does not execute the js until the entire html is loaded
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const table = document.getElementById("table");
+  const alert = document.getElementById("alert");
+  const btn = document.getElementById("add");
+
+  btn.onclick = function () {
+    console.log(title.value + description.value);
+  };
+  function addTodo() {
+    if (title.value === "" || description.value == "") {
+      alert.classList.remove("d-none");
+      alert.innerText = "Title and description are required";
+      return;
+    }
+
+    alert.classList.add("d-none");
+    const row = table.insertRow();
+    row.innerHTML = `
+            <td>${title.value}</td>
+            <td>${description.value}</td>
+            <td></td>
+        `;
+  }
+});
